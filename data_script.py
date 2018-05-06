d23463f05f40780814a0f76f6fe1744bbe41a88f
Jingxixi <yjx941125@gmail.com>
2018-05-06 02:15:04 -0700

    Update data_script with save function

--- data_script.py
+++ data_script.py
@@ -1,20 +1,28 @@
 import pycurl
 import pymongo
+import time
 
 # curl -H "Authorization: Bearer 65bb9f5f762c30c7899f802bb7eb6cd522cfb6b9" https://api.particle.io/v1/devices/360057000351353530373132
 # curl -H "Authorization: Bearer 65bb9f5f762c30c7899f802bb7eb6cd522cfb6b9" https://api.particle.io/v1/devices/360057000351353530373132/events/weight
 
-def save(weight):
-	"""save weight data to mLab
-	"""
-	print(weight)
+def save(data):
+    '''save weight data to mLab'''
+    uri = 'mongodb://root:toor@ds155577.mlab.com:55577/limby'
+    client = pymongo.MongoClient(uri)
+    db = client.get_database()
+    col = db.Data
+    userid = 143744072
+    doc = {}
+    doc["userid"]=userid
+    doc["time"] = int(time.time())
+    doc["value"] = data[29:33]
+    col.insert_one(doc)  
 
 def on_receive(data):
 	"""callback function for Particle stream
 	"""
 	if len(data) > 5:	# omit :OK in response
-		weight = data[29:39]
-		save(weight)
+		save(data)
 
 def sync(deviceId, accessToken):
 	"""sync data from Particle to mLab of given device id
@@ -25,14 +33,14 @@ def sync(deviceId, accessToken):
 	conn.setopt(pycurl.WRITEFUNCTION, on_receive)
 	try:
 		conn.perform()
-	except pycurl.error, error:
+	except pycurl.error as error:
 		errno, errstr = error
 		print(errstr)
 
 def main():
-	deviceId = '360057000351353530373132'
-	accessToken = '65bb9f5f762c30c7899f802bb7eb6cd522cfb6b9'
-	sync(deviceId, accessToken)
+    deviceId = '360057000351353530373132'
+    accessToken = '65bb9f5f762c30c7899f802bb7eb6cd522cfb6b9'
+    sync(deviceId, accessToken)
 
 if __name__ == "__main__":
     main()
