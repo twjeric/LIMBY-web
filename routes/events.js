67e75fa7cb95d254a72a78b9011ae4938dc864a8
Jingxixi <yjx941125@gmail.com>
2018-05-23 01:18:26 -0700

    test

--- routes/events.js
+++ routes/events.js
@@ -10,19 +10,9 @@ router.get('/', auth, async (req, res, next) => {
   // User logged in
   if (req.auth) {
     try {
-      const apiRes = await fetch(config.cloudEndpointUrl + '/api/start', {
-        method: 'POST',
-        headers: {
-          cookie: config.jwtCookie + '=' + req.cookies[config.jwtCookie]
-        }
-      });
-      if (apiRes.status == 500) {
-        res.render('events', { "err": "Server couldn't start saving live data" });
-      }
-      else {
-        res.render('events', { "err": null });
-      }
-    } catch (err) {
+      res.render('events', { "err": null });
+    } 
+    catch (err) {
       res.render('events', { "err": err });
     }
   }
